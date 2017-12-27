2249dcd9136cb2da4106d0eba402f67dd4a256ac
jihobada <jihobada@naver.com>
2017-12-27 15:56:11 +0900

    remove test code

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var net = require('net');
 
 var server = net.createServer(function(socket) {
-  socket.write('Echo server\r\n');
+  socket.write('Echo server running... \r\n');
   socket.pipe(socket);
 });
 
